14b91c086e9440c069fa0fbaec4d0870d5a645f7
Alexey Kuzminich <akuzminich@gmail.com>
2016-07-17 10:31:22 +0800

    Follow paginator links

--- quoka_de/spiders/property.py
+++ quoka_de/spiders/property.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider, Request, FormRequest
 
 
-class PropertyOffersSpider(scrapy.Spider):
+class PropertyOffersSpider(Spider):
     name = "property_offers"
     allowed_domains = ["quoka.de"]
     start_urls = (
@@ -10,7 +10,7 @@ class PropertyOffersSpider(scrapy.Spider):
     )
 
     def apply_filters(self, response):
-        return scrapy.FormRequest.from_response(
+        return FormRequest.from_response(
             response,
             formdata={'classtype': 'of'},
             callback=self.parse_list
@@ -18,5 +18,10 @@ class PropertyOffersSpider(scrapy.Spider):
     parse = apply_filters
 
     def parse_list(self, response):
-        with open('page1.html', 'wt') as f:
-            f.write(response.text)
+        # follow paginator links
+        for a in response.css('ul.sem > li.pageno > a[href]'):
+            yield Request(response.urljoin(a.xpath('./@href').extract_first()),
+                          self.parse_list)
+
+    def parse_page(self, response):
+        pass
